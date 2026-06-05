d00b83a64ff952ebf138cdbe76be705cf4c3ceff
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-06-05 22:39:32 +0300

    fix: add extra link columns

--- src/workspaces/entities/workspaceMembers.entity.ts
+++ src/workspaces/entities/workspaceMembers.entity.ts
@@ -4,15 +4,23 @@ import {
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm';
 import { MembersRoles } from 'src/common/enums/members-roles.enum';
 import { Workspace } from './workspaces.entity';
 import { User } from 'src/users/user.entity';
 
 @Entity('workspace_members')
+@Unique(['workspaceId', 'userId'])
 export class WorkspaceMember {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  workspaceId: number;
+
+  @Column()
+  userId: number;
 
   @ManyToOne(() => Workspace, (workspace) => workspace.workspaceMembers)
   @JoinColumn({ name: 'workspaceId' })
@@ -25,6 +33,6 @@ export class WorkspaceMember {
   @Column({ type: 'enum', enum: MembersRoles })
   role: MembersRoles;
 
-  @Column({ type: 'timestamp', default: 'CURRENT_TIMESTAMP' })
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   joinedAt: Date;
 }
